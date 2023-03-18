cd2bac3a7a5320678d477c699fbde231ac4c6574
AlexArutiunian <arutunanalexandr17@gmail.com>
2023-03-18 19:21:51 +0300

    fix catch...

--- matrix.cpp
+++ matrix.cpp
@@ -15,13 +15,17 @@ int main(){
 
     //if EPS * 0,1 then answer is ok
     EPS = 0.000000001;
+
     std::cin >> n;
-    matrix::math_matrix<T> m(n);
-  
-    std::cin >> m;
     
-    T det = m.det_Gauss(EPS);
+
     
-    std::cout << det << std::endl;
+    matrix::math_matrix<T> M(n);
+    std::cin >> M;
+
+    std::cout << M.det_Gauss() << std::endl;
+
+    
+
     
 }
